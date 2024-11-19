93e277c5b60c8aee67c20432dad6b64675fa1fa3
Adebola <bola@neegles.com>
2024-11-19 09:43:47 +0100

    project setup cleanups

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from "vue";
-import "./assets/main.css";
-import App from "./App.vue";
+import { createPinia } from "pinia";
 
-createApp(App).mount("#app");
+import "./assets/main.css";
+
+import App from "./App.vue";
+import router from "./router";
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from "node:url";
+
 import vue from "@vitejs/plugin-vue";
 import autoprefixer from "autoprefixer";
 
@@ -12,4 +14,9 @@ export default defineConfig({
     },
   },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
